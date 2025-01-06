cb9f9eda95278e27a89e17f107d429954e55ca38
Etienne Moullet <etienne.moullet@gmail.com>
2025-01-06 17:34:30 +0100

    smaller change

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 #TODO : MODIFY THIS FILE ACCORDING TO YOUR NEEDS
 
-DEFAULT_MAIN_PATH = os.path.join(os.path.dirname(__file__), 'Example_DATA')
+DEFAULT_MAIN_PATH = os.path.join(os.path.dirname(__file__), 'Example_DATA') # This should ultimately be the path to your data folder, which should NOT be in the same directory as the code
 
 SESSION_OPTIONS = ["Session 1: Recordings"]
 MODES = ["Recording"]
